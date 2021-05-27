ed13693dbfe843685133e68190bcc0c61bd7ed32
Yuri <48300685+Rilufi@users.noreply.github.com>
2021-05-27 10:50:33 -0300

    Update rilufab.py
    Added secrets

--- rilufab.py
+++ rilufab.py
@@ -2,8 +2,6 @@
 # coding=utf-8
 
 import tweepy
-#import os
-#import random
 from datetime import datetime, timezone, timedelta
 import requests
 import json
@@ -14,7 +12,7 @@ url = "https://api.thecatapi.com/v1/images/search?format=json"
 payload={}
 headers = {
   'Content-Type': 'application/json',
-  'x-api-key': '82dfc9d5-f821-4f67-b8ae-a4dd1af8671d'
+  'x-api-key': cat_key
 }
 
 proxies = {
@@ -22,19 +20,9 @@ proxies = {
 }
 
 response = requests.request("GET", url, headers=headers, data=payload, proxies=proxies)
-
-
-#print(response.text)
-
 todos = json.loads(response.text)
-
-
 site = todos[0].get('url')
-
-
 r = requests.get(site, allow_redirects=True)
- 
-
 open('gato.jpeg', 'wb').write(r.content)
 
 
@@ -43,23 +31,11 @@ data_e_hora_atuais = datetime.now()
 data_e_hora_sao_paulo = data_e_hora_atuais.astimezone(fuso_horario)
 data = data_e_hora_sao_paulo.strftime('%H:%M')
 
-consumer_key = 'RD04L1SMWeyNNx62v1tIW8Lof'
-consumer_secret = 'BZG1fhoy3KKDRVjrxYjrBWjCsbaZQ6MtQYOEphQtkjqYlwbmMU'
-access_token = '1375883846521065473-cfbZAMar1Fa5ygr8el1omyCLzk20Gb'
-access_token_secret = 'OGCZYEsPMJ2GvGrwAjvIZCEpbtn6LN1WBqUWsRKTMijka'
-bearer_token = 'AAAAAAAAAAAAAAAAAAAAAN46OAEAAAAAoQhvZsdmAd9x2vSFh%2Bsa9ThV8TQ%3DWo5GCUA3tg8kgPl3D4Qd9QzWKp0EPZBmhmYxTcw2CE8cTbrcaU'
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-
-#path = r"/home/rilufab/data/cat/"
-#random_cat = random.choice([
-#    x for x in os.listdir(path)
-#    if os.path.isfile(os.path.join(path, x))
-#])
-#print(random_cat)
 
 mystring = f""" Gato Surpresa das {data}"""
 
