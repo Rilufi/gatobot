49672b554db1aa535b64c0d9b0fbcc5797262871
Yuri <48300685+Rilufi@users.noreply.github.com>
2021-05-27 14:50:56 -0300

    Update rilufab.py

--- rilufab.py
+++ rilufab.py
@@ -46,16 +46,19 @@ auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
 
+print(response.text)
+print(CONSUMER_KEY)
+print(CAT_KEY)
 
 mystring = f""" Gato Surpresa das {data}"""
 
 #failsafe to try again in case the image is too large for twitter or any other problem
-try:
-	api.update_with_media('gato.jpeg', mystring)
-except:
-	response2 = requests.request("GET", url, headers=headers, data=payload, proxies=proxies)
-	todos2 = json.loads(response2.text)
-	site2 = todos2[0].get('url')
-	r2 = requests.get(site2, allow_redirects=True)
-	open('gato2.jpeg', 'wb').write(r2.content)
-	api.update_with_media('gato2.jpeg', mystring)
+#try:
+#	api.update_with_media('gato.jpeg', mystring)
+#except:
+#	response2 = requests.request("GET", url, headers=headers, data=payload, proxies=proxies)
+#	todos2 = json.loads(response2.text)
+#	site2 = todos2[0].get('url')
+#	r2 = requests.get(site2, allow_redirects=True)
+#	open('gato2.jpeg', 'wb').write(r2.content)
+#	api.update_with_media('gato2.jpeg', mystring)
