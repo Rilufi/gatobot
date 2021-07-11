8e8e237e1e3c94b66e02567f91ae2635f0e0ab07
Yuri <48300685+Rilufi@users.noreply.github.com>
2021-07-11 18:09:43 -0300

    Add files via upload

---
+++ rtcat.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python
+# coding=utf-8
+
+import tweepy
+import os
+
+#calling secret variables
+CONSUMER_KEY = os.environ["CONSUMER_KEY"]
+CONSUMER_SECRET = os.environ["CONSUMER_SECRET"]
+ACCESS_TOKEN = os.environ["ACCESS_TOKEN"]
+ACCESS_TOKEN_SECRET = os.environ["ACCESS_TOKEN_SECRET"]
+
+
+#connect on twitter
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+api = tweepy.API(auth)
+
+
+#search hashtag and RT
+queries = ["#CatsOfTwitter"]
+for tweet in tweepy.Cursor(api.search, q=queries).items(1):
+    tweet.retweet()
