920562b08780f5395fdef158eacc1eb66d1dc911
Yuri <48300685+Rilufi@users.noreply.github.com>
2021-07-28 13:07:35 -0300

    Update rtcat.py
    add like and follow

--- rtcat.py
+++ rtcat.py
@@ -20,4 +20,6 @@ api = tweepy.API(auth)
 #search hashtag and RT
 queries = ["#CatsOfTwitter"]
 for tweet in tweepy.Cursor(api.search, q=queries).items(1):
+    api.create_favorite(tweet.id)
     tweet.retweet()
+    api.create_friendship(tweet.user.screen_name)
