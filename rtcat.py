2697b9a2b3d8d8ecad8f856a9fc7d33fc1c1ee84
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-04-23 16:01:11 -0300

    Update rtcat.py

--- rtcat.py
+++ rtcat.py
@@ -1,6 +1,3 @@
-#! /usr/bin/env python
-# coding=utf-8
-
 import tweepy
 import os
 
@@ -10,24 +7,21 @@ CONSUMER_SECRET = os.environ["CONSUMER_SECRET"]
 ACCESS_TOKEN = os.environ["ACCESS_TOKEN"]
 ACCESS_TOKEN_SECRET = os.environ["ACCESS_TOKEN_SECRET"]
 
-
 #connect on twitter
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
 
-
 #search hashtag, RT, like and follow
-#in case I want more hashtags, I'll leave the queries list commented
-#also put two filters, one for only RT the original tweet and other for just media content
-#queries = []
-for tweet in tweepy.Cursor(api.search, q="#CatsOfTwitter -filter:retweets filter:media filter:safe").items(1):
-    api.create_favorite(tweet.id)
-    api.create_friendship(tweet.user.screen_name)
-    tweet.retweet()
+#three filters: one for only RT the original tweet, other for just media content and last safe images
 
- 
-for tweet in tweepy.Cursor(api.search, q="#DogsOfTwitter -filter:retweets filter:media filter:safe").items(1):
-    api.create_favorite(tweet.id)
-    api.create_friendship(tweet.user.screen_name)
-    tweet.retweet()
+queries = [#CatsOfTwitter, #DogsOfTwitter, #Caturday, #dog, #cat]
+
+def rtquery(hash):
+    for tweet in tweepy.Cursor(api.search, q=f"{hash} -filter:retweets filter:media filter:safe").items(1):
+        api.create_favorite(tweet.id)
+        api.create_friendship(tweet.user.screen_name)
+        tweet.retweet()
+    
+for query in queries:
+    rtquery(query)
