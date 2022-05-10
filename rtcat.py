7e432c729df015d5ffbf615a3132d9c6fdcbf5a2
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-05-10 15:06:12 -0300

    Update rtcat.py

--- rtcat.py
+++ rtcat.py
@@ -36,12 +36,12 @@ def rtquery(hash):
 pet = ["cat", "dog"]
 error = [100,101,102,200,201,202,203,204,206,207,300,301,302,303,304,305,307,308,400,401,402,403,404,405,406,407,408,409,410,411,412,413,414,415,416,417,418,420,421,422,423,424,425,426,429,431,444,450,451,497,498,499,500,501,502,503,504,506,507,508,509,510,511,521,523,525,599]
 
-site ="https://http."+random.choice(pet)+"/"+str(random.choice(error))
-urllib.request.urlretrieve(site, 'http_pet.jpeg')
+site ="https://http."+random.choice(pet)+"/"+str(random.choice(error))+".jpg"
+urllib.request.urlretrieve(site, 'http_pet.jpg')
 
 today = datetime.date.today() # ex 2015-10-31
 data = today.strftime("%d/%m")
 
 #post on twitter
 mystring = f""" Status http do dia {data}"""
-api.update_with_media('http_pet.jpeg', mystring)
+api.update_with_media('http_pet.jpg', mystring)
