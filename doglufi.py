fcbe5662a0dd5c971d31791876e3691368562ee9
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-05-11 21:32:02 -0300

    Update doglufi.py

--- doglufi.py
+++ doglufi.py
@@ -10,7 +10,7 @@ data_e_hora_atuais = datetime.now()
 data_e_hora_sao_paulo = data_e_hora_atuais.astimezone(fuso_horario)
 data = data_e_hora_sao_paulo.strftime('%H:%M')
 
-mystring = f""" Surprise Dog from {data}
+mystring = f""" {data} Surprise Dog
 #DogsOfTwitter #dogs #DogsOnTwitter"""
 
 def get_random_dog(filename: str='temp') -> None:
