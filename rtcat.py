359e5911ada8cf658abf87eccc4a67b7b2179aa1
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-05-12 14:37:24 -0300

    Update rtcat.py

--- rtcat.py
+++ rtcat.py
@@ -32,27 +32,3 @@ def rtquery(hash):
     
 for query in queries:
     rtquery(query)
-
-today = date.today() # ex 2015-10-31
-data = today.strftime("%d/%m")
-
-#get the time with timezone
-fuso_horario = timezone(timedelta(hours=-3))
-data_e_hora_atuais = datetime.now()
-data_e_hora_sao_paulo = data_e_hora_atuais.astimezone(fuso_horario)
-hora = data_e_hora_sao_paulo.strftime('%H')
-
-
-pet = ["cat", "dog"]
-error = [100,101,102,200,201,202,203,204,206,207,300,301,302,303,304,305,307,308,400,401,402,403,404,405,406,407,408,409,410,411,412,413,414,415,416,417,418,420,421,422,423,424,425,426,429,431,444,450,451,497,498,499,500,501,502,503,504,506,507,508,509,510,511,521,523,525,599]
-
-def http_pet():
-    site ="https://http."+random.choice(pet)+"/"+str(random.choice(error))+".jpg"
-    urllib.request.urlretrieve(site, 'http_pet.jpg')
-    mystring = f""" HTTP status of the day {data}"""
-    api.update_with_media('http_pet.jpg', mystring)
-
-if hora == '12':
-    http_pet()
-else:
-    pass
