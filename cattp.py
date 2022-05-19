e6d0e13372c6247a36ae003ab4d9924255975032
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-05-19 16:09:22 -0300

    Update cattp.py

--- cattp.py
+++ cattp.py
@@ -3,18 +3,8 @@ import os
 import urllib.request
 import random
 from datetime import date
+from auth import api
 
-
-#calling secret variables
-CONSUMER_KEY = os.environ["CONSUMER_KEY"]
-CONSUMER_SECRET = os.environ["CONSUMER_SECRET"]
-ACCESS_TOKEN = os.environ["ACCESS_TOKEN"]
-ACCESS_TOKEN_SECRET = os.environ["ACCESS_TOKEN_SECRET"]
-
-#connect on twitter
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-api = tweepy.API(auth)
 
 #search last entry on specific account, RT and like
 #three filters: one for only RT the original tweet, other for just media content and last safe images
