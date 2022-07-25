b19ec632b2261df115c2236e5097d4e592c5a155
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-07-25 11:32:57 -0300

    Update unfollow.py

--- unfollow.py
+++ unfollow.py
@@ -1,18 +1,19 @@
 import tweepy
 from auth import api
+import sys
 
 class desfollow:
 
     def unfollower(self):
         followers = api.followers_ids(screen_name=api.me().screen_name)
-        print("Followers:", len(followers))
         friends = api.friends_ids(screen_name=api.me().screen_name)
         print("You follow:", len(friends))
-        print("The difference between followers and following is:", len(friends)-len(followers))
-
-        while friends > 3000:
-            for friend in friends[::-1]:
-                if friend not in followers:
+        
+        for friend in friends[::-1]:
+            if friend not in followers:
+                if len(friends) > 3000:
                     api.destroy_friendship(friend) 
-            friends = api.friends_ids(screen_name=api.me().screen_name)
-            print("Now you're following:", len(friends))
+                else:
+                    friends = api.friends_ids(screen_name=api.me().screen_name)
+                    print("Now you're following:", len(friends))
+                    sys.exit()
