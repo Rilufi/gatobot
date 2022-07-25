1b01938b9985453e88b531ff7c182f23796ee868
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-07-25 08:52:35 -0300

    Update unfollow.py

--- unfollow.py
+++ unfollow.py
@@ -3,11 +3,12 @@ from auth import api
 
  
 followers = api.followers_ids(screen_name=api.me().screen_name)
-#print("Followers", len(followers))
+print("Followers", len(followers))
 friends = api.friends_ids(screen_name=api.me().screen_name)
-#print("You follow:", len(friends))
+print("You follow:", len(friends))
 print("The difference between followers and following is:", len(friends)-len(followers))
 
 for friend in friends:
     if friend not in followers:
         api.destroy_friendship(friend) 
+print("Now you're following:", len(friends))
