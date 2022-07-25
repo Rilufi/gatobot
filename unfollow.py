e4ebe5bc058a90871e481a54cf3e8aaec43689b4
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-07-25 08:44:26 -0300

    Update unfollow.py

--- unfollow.py
+++ unfollow.py
@@ -2,9 +2,9 @@ import tweepy
 from auth import api
 
  
-followers = api.get_followers_ids(screen_name=api.me().screen_name)
+followers = api.followers_ids(screen_name=api.me().screen_name)
 #print("Followers", len(followers))
-friends = api.get_friends_ids(screen_name=api.me().screen_name)
+friends = api.friends_ids(screen_name=api.me().screen_name)
 #print("You follow:", len(friends))
 print("The difference between followers and following is:", len(friends)-len(followers))
 
