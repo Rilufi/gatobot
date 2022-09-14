b13e1893cd4e6aa441254f692611a54545937d72
Yuri <48300685+Rilufi@users.noreply.github.com>
2022-09-14 19:38:10 -0300

    Update cattp.py

--- cattp.py
+++ cattp.py
@@ -19,7 +19,7 @@ data = today.strftime("%d/%m")
 
 #search last entry on specific account, RT and like
 #three filters: one for only RT the original tweet, other for just media content and last safe images
-queries = ['CreatureTikToks', 'CatWorkers', 'weirdlilguys', 'PetWorld02', 'genius_dogs', 'gatinarios', 'Thereisnocat_', 'TranslatedCats', 'TweetsOfCats', 'perritos_qctd', 'gatitos_qctd', 'nywolforg', 'hourlywolvesbot', 'HutCat', 'DogsTwt', 'twtCats', 'Bodegacats']
+queries = ['hamstersmp4', 'CreatureTikToks', 'CatWorkers', 'weirdlilguys', 'PetWorld02', 'genius_dogs', 'gatinarios', 'Thereisnocat_', 'TranslatedCats', 'TweetsOfCats', 'perritos_qctd', 'gatitos_qctd', 'nywolforg', 'hourlywolvesbot', 'HutCat', 'DogsTwt', 'twtCats', 'Bodegacats']
 
 def rtquery(hash):
     for tweet in tweepy.Cursor(api.search, q=f"from:{hash} -filter:retweets -filter:replies filter:media filter:safe").items(1):
