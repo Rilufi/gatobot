d0e5e45fba39cd2aed53ff27689b564da71d783c
Yuri <xamexavu@hotmail.com>
2023-04-13 06:54:31 -0300

    Update hepper.py

--- hepper.py
+++ hepper.py
@@ -3,7 +3,6 @@ import random
 
 lines=open('products.txt').read().splitlines()
 status = random.choice(lines)
-
 mystring = f""" Looking for the perfect way to spoil your furry friend? Look no further than Hepper! High-quality pet products are designed with both you and your pet in mind.
 #PetLovers #CatsOfTwitter #PetProducts
 {status}"""
