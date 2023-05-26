ca615f6802eb87d29331688f554ecc877a5b5353
Yuri <xamexavu@hotmail.com>
2023-05-26 14:07:29 -0300

    Update hepper.py

--- hepper.py
+++ hepper.py
@@ -1,13 +1,5 @@
 from auth import api
-import random
 import requests
-from datetime import date, timezone, timedelta, datetime
-
-#get the time with timezone
-fuso_horario = timezone(timedelta(hours=-3))
-data_e_hora_atuais = datetime.now()
-data_e_hora_sao_paulo = data_e_hora_atuais.astimezone(fuso_horario)
-hora = data_e_hora_sao_paulo.strftime('%H')
 
 def bot():
     r = requests.get('https://catfact.ninja/fact')
