1a818a5f9aad1b6cd8579eab531169e1ef5f9025
Yuri <xamexavu@hotmail.com>
2023-05-26 12:21:07 -0300

    Update hepper.py

--- hepper.py
+++ hepper.py
@@ -1,13 +1,39 @@
 from auth import api
 import random
+import requests
+from datetime import date, timezone, timedelta, datetime
 
-lines=open('products.txt').read().splitlines()
-status = random.choice(lines)
-mystring = f""" Looking for the perfect way to spoil your furry friend? Look no further than Hepper! High-quality pet products are designed with both you and your pet in mind.
-#PetLovers #CatsOfTwitter #PetProducts
-{status}"""
+#get the time with timezone
+fuso_horario = timezone(timedelta(hours=-3))
+data_e_hora_atuais = datetime.now()
+data_e_hora_sao_paulo = data_e_hora_atuais.astimezone(fuso_horario)
+hora = data_e_hora_sao_paulo.strftime('%H')
 
-try:
-    api.update_status(mystring)
-except:
-    pass
+def bot():
+    r = requests.get('https://catfact.ninja/fact')
+    data = r.json()
+    quote = f'{data["fact"]}'
+    length = data["length"]
+    if length < 240:
+        api.update_status(quote)
+    else:
+        pass
+
+bot()
+
+def hepper():
+    lines=open('products.txt').read().splitlines()
+    status = random.choice(lines)
+    mystring = f""" Looking for the perfect way to spoil your furry friend? Look no further than Hepper! High-quality pet products are designed with both you and your pet in mind.
+    #PetLovers #CatsOfTwitter #PetProducts
+    {status}"""
+
+    try:
+        api.update_status(mystring)
+    except:
+        pass
+
+   if hora == '12':
+        hepper()
+   else: 
+        pass
