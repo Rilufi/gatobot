42fd57c12a591d7bb9c0e6000a03ff186f517687
Yuri <xamexavu@hotmail.com>
2023-05-26 14:07:10 -0300

    Update hepper.py

--- hepper.py
+++ hepper.py
@@ -20,20 +20,3 @@ def bot():
         pass
 
 bot()
-
-def hepper():
-    lines=open('products.txt').read().splitlines()
-    status = random.choice(lines)
-    mystring = f""" Looking for the perfect way to spoil your furry friend? Look no further than Hepper! High-quality pet products are designed with both you and your pet in mind.
-    #PetLovers #CatsOfTwitter #PetProducts
-    {status}"""
-
-    try:
-        api.update_status(mystring)
-    except:
-        pass
-
-   if hora == '12':
-        hepper()
-   else: 
-        pass
