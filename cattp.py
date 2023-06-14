bb84b1b3c35d97b0559dcde14e965516775baabe
Yuri <xamexavu@hotmail.com>
2023-06-14 17:14:40 -0300

    Update cattp.py

--- cattp.py
+++ cattp.py
@@ -43,7 +43,7 @@ def hepper():
     lines=open('products.txt').read().splitlines()
     status = random.choice(lines)
     mystring = f""" Looking for the perfect way to spoil your furry friend? Look no further than Hepper! High-quality pet products are designed with both you and your pet in mind.
-    #PetLovers #CatsOfTwitter #PetProducts
+#PetLovers #CatsOfTwitter #PetProducts
     {status}"""
 
     try:
