ca0695160e34473914453a4dca31a2c9ce32eb6b
Yuri <xamexavu@hotmail.com>
2023-11-20 18:06:38 -0300

    Update rtcat.py

--- rtcat.py
+++ rtcat.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 import chromedriver_autoinstaller
 from pyvirtualdisplay import Display
 import time
