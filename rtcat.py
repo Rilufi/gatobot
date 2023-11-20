a13eff94fe706bfbf7b3c799a219315a4573e572
Yuri <xamexavu@hotmail.com>
2023-11-20 18:54:08 -0300

    Update rtcat.py

--- rtcat.py
+++ rtcat.py
@@ -6,6 +6,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 import time
+import os
 import chromedriver_autoinstaller
 
 chromedriver_autoinstaller.install()
