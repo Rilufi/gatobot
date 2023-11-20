cd22b2d06882d0a97e2f39b45818c2041e97a541
Yuri <xamexavu@hotmail.com>
2023-11-20 19:00:40 -0300

    Update rtcat.py

--- rtcat.py
+++ rtcat.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException 
 from selenium.common.exceptions import NoSuchElementException
 from webdriver_manager.chrome import ChromeDriverManager
 import time
