90d89756f3f5a68424b760c18aedeeefa229a65e
Yuri <xamexavu@hotmail.com>
2023-11-23 15:36:41 -0300

    Update rtcat.py

--- rtcat.py
+++ rtcat.py
@@ -77,8 +77,6 @@ def obter_codigo_verificacao():
 
     return resultado_final
 
-# Chamar a função e obter o resultado
-
 
 # Define a custom user agent
 user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
