c33ae8deb0bd3419c0ab3bd3ff99ab41dc6f9633
Yuri <xamexavu@hotmail.com>
2024-03-24 07:32:16 -0300

    Update auth.py

--- auth.py
+++ auth.py
@@ -5,7 +5,8 @@ client = tweepy.Client(
     consumer_key = os.environ.get("CONSUMER_KEY"),
     consumer_secret = os.environ.get("CONSUMER_SECRET"),
     access_token = os.environ.get("ACCESS_TOKEN"),
-    access_token_secret = os.environ.get("ACCESS_TOKEN_SECRET")
+    access_token_secret = os.environ.get("ACCESS_TOKEN_SECRET"),
+    wait_on_rate_limit=True
 )
 
 #calling secret variables
