14d465e4eac0e3c05d74e34e2d42c4d3beedb78e
Yuri <xamexavu@hotmail.com>
2024-05-14 20:35:07 -0300

    Update boturitter.py

--- boturitter.py
+++ boturitter.py
@@ -159,7 +159,7 @@ def download_random_image():
 # Function to post AI-generated cat tweet
 def post_ai_generated_cat_tweet():
     data = datetime.now().astimezone(timezone(timedelta(hours=-3))).strftime('%H:%M')
-    response_gemini = gemini_image("Write a funny and/or cute tweet about this ai-generated cat image with hashtags","fakecat.jpg")
+    response_gemini = gemini_image("Write a funny and/or cute tweet about this ai-generated cat image with twitter related hashtags","fakecat.jpg")
     if response_gemini == None:
         response_gemini = "#AI #GAN #thesecatsdonotexist"
     else:
@@ -173,7 +173,7 @@ def post_ai_generated_cat_tweet():
 # Function to post random cat tweet
 def post_random_cat_tweet():
     data = datetime.now().astimezone(timezone(timedelta(hours=-3))).strftime('%H:%M')
-    response_gemini = gemini_image("Write a funny and/or cute tweet about this cat image with hashtags",'cat_image.jpg')
+    response_gemini = gemini_image("Write a funny and/or cute tweet about this cat image with twitter related hashtags",'cat_image.jpg')
     if response_gemini == None:
         response_gemini = "#CatsOfTwitter #cats #CatsOnTwitter"
     else:
@@ -187,7 +187,7 @@ def post_random_cat_tweet():
 # Function to post random dog tweet
 def post_random_dog_tweet():
     data = datetime.now().astimezone(timezone(timedelta(hours=-3))).strftime('%H:%M')
-    response_gemini = gemini_image("Write a funny and/or cute tweet about this dog image with hashtags",'dog_image.jpg')
+    response_gemini = gemini_image("Write a funny and/or cute tweet about this dog image with twitter related hashtags",'dog_image.jpg')
     if response_gemini == None:
         response_gemini = "#DogsOfTwitter #dogs #DogsOnTwitter"
     else:
