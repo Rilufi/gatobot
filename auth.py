9060e98e4fa8acaeb7e6393e767a3c57dd30a175
Yuri <xamexavu@hotmail.com>
2024-05-24 21:34:08 -0300

    Update auth.py

--- auth.py
+++ auth.py
@@ -6,7 +6,7 @@ client = tweepy.Client(
     consumer_secret = os.environ.get("CONSUMER_SECRET"),
     access_token = os.environ.get("ACCESS_TOKEN"),
     access_token_secret = os.environ.get("ACCESS_TOKEN_SECRET"),
-    wait_on_rate_limit=True
+    wait_on_rate_limit=False
 )
 
 #calling secret variables
@@ -18,4 +18,4 @@ ACCESS_TOKEN_SECRET = os.environ["ACCESS_TOKEN_SECRET"]
 #connect on twitter
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-api = tweepy.API(auth, wait_on_rate_limit=True)
+api = tweepy.API(auth, wait_on_rate_limit=False)
